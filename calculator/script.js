b5f866d7168c8ef52d977054d2f9ebcaf8459823
Neha Kumari <121566993+nkashyap01@users.noreply.github.com>
2023-08-12 23:55:03 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,15 @@
+// document.querySelector(".equal").addEventListener("click", function () {
+//   const randColor = Math.floor(Math.random() * 16777215).toString(16);
+//   document.querySelector(".container").style.backgroundColor = "#" + randColor;
+
+//   const randcolor = Math.floor(Math.random() * 16777215).toString(16);
+//   document.querySelector(".calculator").style.backgroundColor = "#" + randcolor;
+
+//   const allBtn = document.querySelectorAll(".c");
+
+//   allBtn.forEach((btn) => {
+//     const randCcolor = Math.floor(Math.random() * 16777215).toString(16);
+//     btn.style.backgroundColor = "#" + randCcolor;
+    
+//   });
+// });
